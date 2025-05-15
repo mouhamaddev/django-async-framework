44ea2e9909daba5bfb8424d16f76fd04256c1f3c
Mohammad Massri <mouhamaddev04@gmail.com>
2025-05-16 00:56:15 +0300

    update project structure

--- async_framework/async_framework/views.py
+++ async_framework/async_framework/views.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 import asyncio
 from django.http import HttpResponse
 from django.http import JsonResponse
