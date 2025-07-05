6b9794ad4e1a2206eb63041d28172f6e8c45e136
Mohammad Massri <mouhamaddev04@gmail.com>
2025-07-06 00:00:43 +0300

    add class-level docstring to AsyncView

--- async_framework/views/core.py
+++ async_framework/views/core.py
@@ -9,10 +9,33 @@ from .service_resolver import _resolve_services
 
 class AsyncView(View):
     """
-    Enforces all HTTP method handlers
-    to be defined as async functions.
-    # TODO: Update the comment above to better reflect the usage details.
-    # TODO: And add more details about the services attribute and its usage.
+    AsyncView is the base class for writing fully asynchronous Django views.
+
+    1) All HTTP method handlers must be defined as async def.  
+        If you use a regular function by mistake, a TypeError is raised to protect the
+        async event loop.
+
+    2) Define an async_setup() method to run custom async logic before the request is handled
+        for preloading data, fetching related models, or any per-request setup.
+        Usage of this hook is optional, but recommended for async-safe operations.
+
+    3) Declare a services dictionary (or override services_attr) with factories for services
+        your view depends on.  
+        They're resolved once per request and attached to the view instance, so your code
+        stays clean and DRY.
+    
+    Usage:
+        class MyView(AsyncView):
+            services = {
+                "reporting": lambda: ReportingService(),
+            }
+
+            async def async_setup(self, request, *args, **kwargs):
+                self.user = await User.objects.aget(pk=request.user.pk)
+
+            async def get(self, request):
+                data = await self.services.reporting.get_report(self.user)
+                return JsonResponse(data)
     """
 
     services_attr = "services"  # Default services attribute name, can be overridden in subclasses.
