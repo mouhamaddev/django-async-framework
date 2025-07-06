74dc526aa832e1a98aa65ce417e6f9a9dcd8d2d3
Mohammad Massri <mouhamaddev04@gmail.com>
2025-07-06 13:38:27 +0300

    add DATABASES config to test settings to avoid ImproperlyConfigured error

--- tests/django_config.py
+++ tests/django_config.py
@@ -1,4 +1,7 @@
+import django
+
 from django.conf import settings
+
 
 def configure():
     if not settings.configured:
@@ -10,4 +13,11 @@ def configure():
             ALLOWED_HOSTS=['*'],
             MIDDLEWARE=[],
             INSTALLED_APPS=[],
+            DATABASES={
+                "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                    "NAME": ":memory:", # For tests
+                }
+            },
         )
+    django.setup()
